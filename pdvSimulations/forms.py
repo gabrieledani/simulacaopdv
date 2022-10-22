c980a9276613a0c4b87f5a4967ebd7918b6b7813
Gabriele <gdani@ucs.br>
2022-10-22 18:56:16 -0300

    22-10

--- pdvSimulations/forms.py
+++ pdvSimulations/forms.py
@@ -2,22 +2,42 @@ from django import forms
 from .models import Simulacao,Itens_Simulacao
 
 class NovaSimulacao(forms.ModelForm):
+    tipo_simulacao = forms.ChoiceField(label="Tipo de Proposta:", initial='',widget=forms.Select(attrs={'class':'form-select'}),
+                choices=((1, "Tipo 1"), (2, "Tipo 2"), (3, "Tipo 3"), (4, "Tipo 4"), (5, "Outro")), required=True)
+
     class Meta:
         model = Simulacao
-        fields = ('dt_simulacao','cod_rep','cod_cliente','cod_empr')#,'situacao')
+        fields = ('dt_simulacao','cod_rep','cod_cliente','cod_empr','aprovada','observacao','tipo_simulacao','txt_verba1','vlr_verba1','txt_verba2','vlr_verba2','txt_verba3','vlr_verba3','txt_verba4','vlr_verba4','txt_verba5','vlr_verba5')
         labels = {
             'dt_simulacao':'Data:',
             'cod_rep':'Representante:',
             'cod_cliente':'Cliente:',
-            'cod_empr':'Empresa:'
-            #'situacao':'Ativo'
+            'cod_empr':'Empresa:',
+            'aprovada':'Aprovada:',
+            'observacao':'Observação:',
+            'txt_verba1':'Verba Adicional 1:',
+            'txt_verba2':'Verba Adicional 2:',
+            'txt_verba3':'Verba Adicional 3:',
+            'txt_verba4':'Verba Adicional 4:',
+            'txt_verba5':'Verba Adicional 5:'
         }
         widgets = {
             'dt_simulacao':forms.DateInput(attrs={'type':'date','class':'form-control'}),
             'cod_rep':forms.NumberInput(attrs={'class':'form-control'}),
             'cod_cliente':forms.NumberInput( attrs={'class':'form-control'}),
-            'cod_empr':forms.NumberInput(attrs={'class':'form-control'})
-            #'situacao':forms.CheckboxInput(attrs={'class':'form-check-input'})
+            'cod_empr':forms.NumberInput(attrs={'class':'form-control'}),
+            'aprovada':forms.CheckboxInput(attrs={'class':'form-check-input'}),
+            'observacao':forms.Textarea(attrs={'class':'form-control'}),
+            'txt_verba1':forms.TextInput(attrs={'class':'form-control'}),
+            'vlr_verba1':forms.NumberInput(attrs={'class':'form-control'}),
+            'txt_verba2':forms.TextInput(attrs={'class':'form-control'}),
+            'vlr_verba2':forms.NumberInput(attrs={'class':'form-control'}),
+            'txt_verba3':forms.TextInput(attrs={'class':'form-control'}),
+            'vlr_verba3':forms.NumberInput(attrs={'class':'form-control'}),
+            'txt_verba4':forms.TextInput(attrs={'class':'form-control'}),
+            'vlr_verba4':forms.NumberInput(attrs={'class':'form-control'}),
+            'txt_verba5':forms.TextInput(attrs={'class':'form-control'}),
+            'vlr_verba5':forms.NumberInput(attrs={'class':'form-control'}),
         }
 
 class NovoItem(forms.ModelForm):
--- pdvSimulations/models.py
+++ pdvSimulations/models.py
@@ -1,4 +1,5 @@
 from email.policy import default
+from unittest.util import _MAX_LENGTH
 from django.db import models
 
 class Simulacao(models.Model):
@@ -8,6 +9,19 @@ class Simulacao(models.Model):
     cod_cliente = models.IntegerField()
     cod_empr = models.IntegerField(default=1)
     situacao = models.BooleanField(default=True)
+    aprovada = models.BooleanField(default=False)
+    observacao = models.CharField(max_length=200,blank=True)
+    tipo_simulacao = models.IntegerField(choices=((1, "Tipo1"),(2, "Tipo2"),(3, "Tipo3"),(4, "Tipo4"),(5, "Outro")),default=1)
+    txt_verba1 = models.CharField(max_length=20,blank=True)
+    vlr_verba1 = models.FloatField(null=True)
+    txt_verba2 = models.CharField(max_length=20,blank=True)
+    vlr_verba2 = models.FloatField(null=True)
+    txt_verba3 = models.CharField(max_length=20,blank=True)
+    vlr_verba3 = models.FloatField(null=True)
+    txt_verba4 = models.CharField(max_length=20,blank=True)
+    vlr_verba4 = models.FloatField(null=True)
+    txt_verba5 = models.CharField(max_length=20,blank=True)
+    vlr_verba5 = models.FloatField(null=True)
     
     # def __str__(self):
     #     return self.cod_simulacao
@@ -19,7 +33,7 @@ class Itens_Simulacao(models.Model):
     cod_mascara = models.CharField(max_length=20,blank=True)
     qtde = models.IntegerField()
     valor = models.FloatField()
-    ean_item = models.CharField(max_length=20)
+    ean_item = models.CharField(max_length=20,blank=True)
     situacao = models.BooleanField(default=True)
 
     def __str__(self):
