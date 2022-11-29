23a2f73a71f830724a4e7e7cb8f8b85ae4319aac
Gabriele Dani <gabrieledani@gmail.com>
2022-11-29 16:44:34 -0300

    new main

---
+++ README.md
@@ -0,0 +1,15 @@
+Prerequisites
+Python
+virtualenv pip install virtualenv
+Installing
+Passo a passo para a geração de um novo executável, caso venham a ser feita modificações no código.
+
+Criar ambiente virtual: python -m venv venv
+
+Ativar ambiente virtual: .\venv\Scripts\activate
+
+(Desativar quando necessário) deactivate
+
+Instalar dependências pip install -r .\requirements.txt
+
+Depois executa o main.py
--- main.py
+++ main.py
@@ -1,3 +1,22 @@
+import os
 import webbrowser
+import asyncio
 
-webbrowser.open('http://127.0.0.1:8000/')
+
+# RUNSERVER
+async def runserver():
+    path = os.getcwd()
+    os.chdir(path)
+    os.system("py manage.py runserver")
+
+# OPEN BROWSER
+def openproject():
+    webbrowser.open_new_tab("http://127.0.0.1:8000/")
+
+# EXECUTE PROGRAM
+async def main():
+    task1 = asyncio.create_task(runserver())
+    openproject()
+    await task1
+
+asyncio.run(main())
